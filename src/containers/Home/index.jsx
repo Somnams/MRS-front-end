0b89fd9ebbdb3587f00d99c4794fb2338fb3ab98
Somnams <3244345132@qq.com>
2021-04-28 18:41:57 +0800

    🍃 axios config

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
+import axios from '../../request';
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    componentDidMount() {
+        axios.get('/music/1')
+            .then(({data}) => {
+                console.log(data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
     render() {
         return (
             <div>
---
+++ src/request/index.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import {message} from 'antd';
+
+let hide = null;
+
+axios.defaults.baseURL = 'http://127.0.0.1:5000/api';
+
+// Request interceptors
+axios.interceptors.request.use(config => {
+    const token = window.localStorage.getItem('mrsToken');
+    hide = message.loading('Loading', 0);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
+// Response interceptors
+axios.interceptors.response.use(response => {
+    hide();
+    return response;
+}, error => {
+    if (error.response) {
+        switch (error.response.status) {
+            case 401:
+                message.error('401: Please login');
+                break;
+            case  403:
+                message.error('403: Forbidden');
+                break;
+            case 404:
+                message.error('404: Not Found');
+                break;
+            default:
+                break;
+        }
+    } else if (error.request) {
+        message.error('OPTIONS get Error');
+    } else {
+        console.error(error.message);
+    }
+    return Promise.reject(error);
+});
+
+export default axios;
