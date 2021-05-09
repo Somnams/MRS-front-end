a2b9d5c5d6f499b929d01bc6d497930bae1f5428
Somnams <3244345132@qq.com>
2021-05-09 17:20:06 +0800

    perf: ⚡️ logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,36 @@
 import * as React from 'react';
 import {Layout, Button} from 'antd';
 import {LogoutOutlined} from '@ant-design/icons';
+import {withRouter} from 'react-router-dom';
 import './index.scss';
 
-export default class Header extends React.Component {
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            disabled: false
+        };
+    }
+
+    logout() {
+        window.localStorage.removeItem('mrs-storage');
+        this.props.history.push('/login');
+    }
+
+
     render() {
         return (
             <Layout.Header className={'header'}>
-                <Button type={'primary'} ghost icon={<LogoutOutlined />}/>
+                <Button
+                    type={'primary'} ghost
+                    icon={<LogoutOutlined />}
+                    onClick={this.logout.bind(this)}
+                />
             </Layout.Header>
         );
     };
 };
+
+
+export default withRouter(Header);
