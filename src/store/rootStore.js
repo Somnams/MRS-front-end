d9b0d002463486bbf0d15adf371875e23c1c09ee
Somnams <3244345132@qq.com>
2021-05-09 18:08:33 +0800

    fix: 🐛 save userId & username

--- src/store/rootStore.js
+++ src/store/rootStore.js
@@ -1,6 +1,15 @@
 import {observable} from 'mobx';
 
 export default observable({
-    userId: 1,
-    username: ''
+    // user_id: window.localStorage.getItem('madblog-token') ? JSON.parse(atob(window.localStorage.getItem('madblog-token').split('.')[1])).user_id : 0
+    userId: window.localStorage.getItem('mrs-storage')
+        ?
+        JSON.parse(atob(window.localStorage.getItem('mrs-storage').split('.')[1]))['user_id']
+        :
+        0,
+    username: window.localStorage.getItem('mrs-storage')
+        ?
+        JSON.parse(atob(window.localStorage.getItem('mrs-storage').split('.')[1]))['user_name']
+        :
+        '',
 });
