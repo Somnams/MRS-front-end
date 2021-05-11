a0e11527cd55c5f452cb18e59623d2c015267ea0
Somnams <3244345132@qq.com>
2021-05-11 17:02:29 +0800

    refactor: 💡 new home view

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import MusicCard from '../../components/MusicCard';
+import { withTranslation } from 'react-i18next';
 import './index.scss';
 
-export default class Home extends React.Component {
+@withTranslation()
+class Home extends React.Component {
     constructor(props) {
         super(props);
     }
@@ -9,8 +12,21 @@ export default class Home extends React.Component {
     render() {
         return (
            <div className={'page home'}>
-               Home view
+               <div>
+                   <p className={'home-p'}>飙升榜</p>
+                   <MusicCard />
+               </div>
+               <div>
+                   <p className={'home-p'}>新歌榜</p>
+                   <MusicCard />
+               </div>
+               <div>
+                   <p className={'home-p'}>原创榜</p>
+                   <MusicCard />
+               </div>
            </div>
         );
     }
-};
+}
+
+export default Home;
